c61d6e296a040aacdf95c0424f81571bb481c39d
Aaron Kable <aaronkable@gmail.com>
2013-08-12 23:08:01 +0800

    update form to take prefill

--- pvy_ota_muncher/index.php
+++ pvy_ota_muncher/index.php
@@ -1,21 +1,27 @@
+
+<?php
+#
+#	pvyParts
+#	OTA Checker script
+#
+
+# variables
+$Device = htmlspecialchars($_GET['device']);
+$Ver = htmlspecialchars($_GET['version']) ;
+$Download= htmlspecialchars($_GET['dlurl']) ;
+$Md5= htmlspecialchars($_GET['md5']) ;
+$Password = htmlspecialchars(hash('md5', $_GET['pass'])); 
+
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>OTA Muncher!</title>
 	<link rel="stylesheet" type="text/css" href="view.css" media="all">
-		<script type="text/javascript" src="view.js"></script>
 </head>
 <body id="main_body">
-<?php
-	session_start();
-	$deviceName = $_GET['device'];
-	$md5Value = $_GET['md5'];
-	$romVersion = $_GET['version'];
-	$directLink = $_GET['device'];
-	$password = $_GET['md5'];
-?>
-
 	<img id="top" src="top.png" alt="">
 		<div id="form_container">
 			<h1>
@@ -31,31 +37,31 @@
 					<li id="li_1">
 						<p>Device <cap>(eg. Maguro)</cap></p>
 						<div>
-							<input type="text" id="device" name="device" size="50" value="<?php echp $deviceName;?>" />
+							<input type="text" id="device" name="device" size="50" value="<?php echo $Device; ?>" />
 						</div>
 					</li>
 					<li id="li_2">
 						<p>Version <cap>(eg. Milestone1.2013.07.18)</cap></p>
 						<div>
-							<input type="text" id="version" name="otaname" size="50" value="<?php echo $RomVersion; ?>" />
+							<input type="text" id="version" name="otaname" size="50" value="<?php echo $Ver; ?>" />
 						</div>
 					</li>
 					<li id="li_3">
 						<p>Download URL <cap>(Direct Link!)</cap></p>
 						<div>
-							<input type="text" name="dlurl" size="50" value="<?php echo $directLink; ?>"/>
+							<input type="text" name="dlurl" size="50" value="<?php echo $Download; ?>"/>
 						</div>
 					</li>
 					<li id="li_4">
 						<p>MD5 Hash <cap>(of update zip)</cap></p>
 						<div>
-							<input type="text" id="md5" name="md5" size="50" value="<?php echo $md5Value; ?>"/>
+							<input type="text" id="md5" name="md5" size="50" value="<?php echo $Md5; ?>"/>
 						</div>
 					</li>
 					<li id="li_5">
 						<p>Password <cap>(if you dont know then go away!)</cap></p>
 						<div>
-							<input type="password" name="pass" size="50" value="<?php echo $password; ?>"/>
+							<input type="password" name="pass" size="50" value="<?php echo $Password; ?>"/>
 						</div>
 					</li>
 					<li>
