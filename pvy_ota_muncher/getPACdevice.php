dab932c563a1b50728ecd93e22c2ae0df2cab760
Aaron Kable <aaronkable@gmail.com>
2013-10-16 14:13:02 +0800

    cards

--- pvy_ota_muncher/getPACdevice.php
+++ pvy_ota_muncher/getPACdevice.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
 <?php
 include ("config.php");
 
@@ -26,14 +23,7 @@ if (mysqli_connect_errno ()) {
 // OTA Checker script sql database reqd
 //
 
-$Device = htmlspecialchars ( $_GET ['device'] );
 
-if ($Password == $Pass){
-	$perm = true;
-} else {
-	$perm = false;
-}
-$DEBUG = false;
 
 $con = mysqli_connect ( $dbhost, $dbuser, $dbpass, $dbname );
 // Check connection
@@ -41,32 +31,77 @@ if (mysqli_connect_errno ()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error ();
 }
 
-?>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Get PAC!</title>
-	<link rel="stylesheet" type="text/css" href="view.css" media="all">
+		
+				if ($Device == "") {
+					echo "<p> Please Select Your device from the List </p>";
+				} else {
+						
+					$sql = "SELECT * FROM (`nightly`) WHERE (`device`) = '" . $Device . "'";
+					$sqln = call_sql ( $con, $sql );
+					$sql = "SELECT * FROM (`stable`) WHERE (`device`) = '" . $Device . "'";
+					$sqls = call_sql ( $con, $sql );
+					$sql = "SELECT * FROM (`unofficial`) WHERE (`device`) = '" . $Device . "'";
+					$sqlu = call_sql ( $con, $sql );
+					echo "<div class=\"row span12\" id=\"commits\">";
+					if (mysqli_num_rows( $sqls ) > 0) {
+				
+						
+##<div class="col-lg-4">
+##<h3>Commit Title <span>Time</span></h3>
+##<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+##<a class="btn btn-default" href="#">View details »</a>
+##<span>
+##<span class="ui-icon ui-icon-script ui-widget-header"></span> 42 Modified Files <br>
+##<span class="ui-icon ui-icon-person ui-widget-header"></span>Author
+##</span>
+##</div>				
 
-</head>
-<body id="main_body">
-	<img id="top" src="top.png" alt="">
-		<div id="form_container">
-			<img style="float: middle"
-				src="http://www.pac-rom.com/images/logo.png" alt="PAC-man logo" />
-			<h2><?php $Device ?></h2>
-			
-	        <form method="post">
-				<?php 
-				
-				
-				
+						while ( $row = mysqli_fetch_array ( $sqls ) ) {
+							echo "\n<div class=\"col-lg-4\">";
+							echo "<h3>Stable<span>" . $row ['device'] . "</span></h3>";
+							echo "<p>" . $row ['version'] . "</p>";
+							echo "<a class=\"btn btn-default\" href=\"" . $row ['url'] . "\">Download ></a>";
+							echo "<span>";
+							echo "<span class=\"ui-icon ui-icon-script ui-widget-header\"></span> date of release <br>";
+							echo "</span>";
+							echo "</div>";
+						}
+						
+					} else {
+						#echo "<caption>No Stable Roms found for $Device</caption>";
+					}
+					if (mysqli_num_rows( $sqln ) > 0) {
+						while ( $row = mysqli_fetch_array ( $sqln ) ) {
+							echo "\n<div class=\"col-lg-4\">";
+							echo "<h3>Nightly<span>" . $row ['device'] . "</span></h3>";
+							echo "<p>" . $row ['version'] . "</p>";
+							echo "<a class=\"btn btn-default\" href=\"" . $row ['url'] . "\">Download ></a>";
+							echo "<span>";
+							echo "<span class=\"ui-icon ui-icon-script ui-widget-header\"></span> date of release <br>";
+							echo "</span>";
+							echo "</div>";
+						}
+					} else {
+					#echo "<caption>No Nightly Roms found for $Device</caption>";
+					}
+					if (mysqli_num_rows( $sqlu ) > 0) {
+						while ( $row = mysqli_fetch_array ( $sqlu ) ) {
+							echo "\n<div class=\"col-lg-4\">";
+							echo "<h3>Unofficial<span>" . $row ['device'] . "</span></h3>";
+							echo "<p>" . $row ['version'] . "</p>";
+							echo "<a class=\"btn btn-default\" href=\"" . $row ['url'] . "\">Download ></a>";
+							echo "<span>";
+							echo "<span class=\"ui-icon ui-icon-script ui-widget-header\"></span> date of release <br>";
+							echo "</span>";
+							echo "</div>";
+						}
+						
+					echo "</div>";
+					} else {
+					#echo "<caption>No Unofficial Roms found for $Device</caption>";
+					}
+					}
 				
 				
 				
 				?>
-			<footer><br/>Â© pvyParts 2013</footer>
-
-			</form>
-		</div> <img id="bottom" src="bottom.png" alt="">
-
-</body>
-</html>
